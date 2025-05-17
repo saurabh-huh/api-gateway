947f9df16f1fcedde482a8c982510a0908923294
Saurabh-BigOh <saurabh-bigoh@Saurabh-BigOhs-MacBook-Air.local>
2025-05-18 03:11:05 +0530

    feature: add exception handler

---
+++ src/main/java/com/fresco/api_gateway/exceptionHandler/FrescoRestControllerAdvice.java
@@ -0,0 +1,65 @@
+package com.fresco.api_gateway.exceptionHandler;
+
+
+import com.fresco.api_gateway.exceptionHandler.enums.ErrorCodes;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+
+
+@RestControllerAdvice
+@Slf4j
+public class FrescoRestControllerAdvice extends ResponseEntityExceptionHandler {
+
+	private final MessageSource messageSource;
+
+	private final Locale currentLocale = LocaleContextHolder.getLocale();
+
+	@Autowired
+	public FrescoRestControllerAdvice(final MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Object> handleAllExceptionMethod(final MethodArgumentNotValidException ex, final WebRequest requset) {
+		log.info("*** [FrescoRestControllerAdvice] :: handleAllExceptionMethod : Exception : {} ***", ex.getMessage());
+		ExceptionResponse exceptionMessageObj = new ExceptionResponse();
+		StringBuilder sb = new StringBuilder();
+		List<FieldError> fieldErrors = (ex).getBindingResult().getFieldErrors();
+		if(!fieldErrors.isEmpty()) {
+			sb.append(fieldErrors.get(0).getDefaultMessage());
+		}
+		exceptionMessageObj.setStatus(HttpStatus.BAD_REQUEST.value());
+		exceptionMessageObj.setCode(ErrorCodes.NOT_NULL_VALIDATION.getValue());
+		exceptionMessageObj.setMessage(sb.toString());
+		exceptionMessageObj.setError(ex.getClass().getCanonicalName());
+		exceptionMessageObj.setPath(((ServletWebRequest) requset).getRequest().getServletPath());
+		exceptionMessageObj.setType(ErrorCodes.INVALID_DATA.name());
+		return new ResponseEntity<>(exceptionMessageObj, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(BusinessException.class)
+	public ResponseEntity<ExceptionResponse> handleBusinessException(final BusinessException exc,final WebRequest requset) {
+		String errorMessage = messageSource.getMessage(exc.getMessage(), exc.getArgs(), currentLocale);
+		return ResponseEntity.status(Objects.nonNull(exc.getStatus()) ? exc.getStatus() : HttpStatus.BAD_REQUEST)
+				.body(new ExceptionResponse(exc.getStatus().value(), exc.getStatus().getReasonPhrase(), errorMessage,
+						((ServletWebRequest) requset).getRequest().getServletPath(), exc.getCode(), exc.getMessage()));
+	}
+
+}
---
+++ src/main/java/com/fresco/api_gateway/exceptionHandler/enums/ErrorCodes.java
@@ -0,0 +1,32 @@
+package com.fresco.api_gateway.exceptionHandler.enums;
+
+public enum ErrorCodes {
+	INVALID_DATA("FK0001"),
+	NOT_NULL_VALIDATION("FK0002"),
+	FK_EXCEPTION("FK0003"),
+	CONNECTION_FAILED("FK0004"),
+	ENCRYPTION_FAILED("FK0005"),
+	INCORRECT_CREDENTIALS("FK0006"),
+	USER_NOT_FOUND("FK0007");
+
+
+
+
+
+
+
+
+	private String value;
+
+	public String getValue() {
+		return value;
+	}
+
+
+	private ErrorCodes(String value) {
+		this.value = value;
+	}
+
+	
+	
+}
